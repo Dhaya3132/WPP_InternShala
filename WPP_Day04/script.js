21f79e9152359dd0587e3ce133b6cedf92fe95b2
Dhaya3132 <dhayalanu103@gmail.com>
2024-05-16 18:58:55 +0530

    Day four

---
+++ WPP_Day04/script.js
@@ -0,0 +1,8 @@
+const today = new Date();
+const cmas = new Date(today.getFullYear(), 11, 25);
+console.log(cmas);
+if (today.getMonth() == 11 && today.getDate() > 25) {
+    cmas.setFullYear(cmas.getFullYear() + 1);
+}
+const one_day = 1000 * 60 * 60 * 24;
+console.log(`${Math.ceil((cmas.getTime() - today.getTime()) / one_day)} days left until Christmas!`); 
